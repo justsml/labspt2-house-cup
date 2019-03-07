fb17c93cbac1f135d9282d22e646db18ca29c6a7
Ashwin <aswinjeganath@gmail.com>
2019-03-07 09:09:08 +0530

    Schools model and controller implemented

--- api/server.js
+++ api/server.js
@@ -3,29 +3,30 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const logger = require('morgan');
 const helmet = require('helmet');
-const { User } = require('../Models');
+const { User, School } = require('../Models');
 const sequelize = require('../sequelize');
 
+School.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+User.hasMany(School, {
+  foreignKey: 'userId',
+});
+
+sequelize.sync();
 const userRouter = require('../controllers/routes/user_routes');
-// const {User} = require('../Models');
-// const sequelize = require('../sequelize');
-
-// sequelize.sync();
-
-// async function createDummyUser() {
-//   await User.create({ firstName: 'Ashwin' , lastName: 'Sundaran', email: 'ashwin@yahoo.com', password: 'abcde'})
-// }
-// createDummyUser();
+const schoolsRouter = require('../controllers/routes/schools');
 const server = express();
 
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(helmet());
 server.use(logger('tiny'));
 server.use(cors());
 
-
 server.use('/users', userRouter);
+server.use('/schools', schoolsRouter);
 
 server.get('/', (req, res) => {
   res.send(`Server is up and running now.`);
