51608543fc83873653ae6ea1348028e4e3a222d4
louislee7 <7.louislee@gmail.com>
2019-03-21 16:53:20 -0700

    finished landing page styling

--- HouseCup/house-cup-app/src/sub-components/landingPage.js
+++ HouseCup/house-cup-app/src/sub-components/landingPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LandingImg from '../images/realestate.png';
 import Dumbledore from '../images/dumbledore.jpg';
-import IconPhone from '../images/icon-phone.svg';
-import IconChart from '../images/icon-chart.svg';
-import IconTeacher from '../images/icon-teacher.svg';
+import { ReactComponent as IconPhone } from '../images/icon-phone.svg';
+import { ReactComponent as IconChart } from '../images/icon-chart.svg';
+import { ReactComponent as IconTeacher } from '../images/icon-teacher.svg';
 
 import schoolsTestData from '../mock data/schools';
 import auth from '../Auth';
@@ -35,38 +35,35 @@ class LandingPage extends React.Component {
         window.location.reload();
     }
 
-  signOut = () => {
+    signOut = () => {
         auth.logout();
         this.props.history.replace('/');
-  }
+    }
 
     render() {
-        // console.log(this.props.auth.login)
         return (
             <div className='landing-page'>
                 <header className='landing-page-header'>
-                    <NavLink to='/'>
-                        <div className="house-cup-title">House Cup Tracker</div>
-                    </NavLink>
+                    <div className="house-cup-title">House Cup Tracker</div>
                     <div className='button-container'>
-                        <NavLink to='/signup' style={{ textDecoration: "none", color: "inherit" }}>
-                            <button onClick={this.handleSubmit} className="button signup-button">Sign up</button>
-                        </NavLink>
-                          {
-                             !auth.isAuthenticated() &&
-                               <button onClick={auth.login}
-                                       className="button login-button">Sign In</button>
-                           }
-                           {
-                                auth.isAuthenticated() &&
-                                <div>
+
+                        {
+                            !auth.isAuthenticated() &&
+                            <button onClick={auth.login}
+                                className="button login-button">Sign Up/Log In</button>
+                        }
+                        {
+                            auth.isAuthenticated() &&
+                            <div>
                                 <label className="mr-2 text-white">{auth.getProfile().name}</label>
-                                <button className="btn btn-dark" onClick={() => {this.signOut()}}>Sign Out</button>
-                                </div>
-                            }
-                        {/* <NavLink to='/dashboard' style={{ textDecoration: "none", color: "inherit" }}>
-                            <button onClick={this.handleSubmit} className="button login-button">Log in</button>
-                        </NavLink> */}
+                                <button
+                                    className="button logout-button"
+                                    onClick={() => { this.signOut() }}>
+                                    Sign Out
+                                </button>
+                            </div>
+                        }
+
                     </div>
                 </header>
                 <div className='landing-page-block landing-page-block-1'>
@@ -78,24 +75,25 @@ class LandingPage extends React.Component {
                 </div>
                 <div className='landing-page-block landing-page-block-2'>
                     <div className='feature-1 feature'>
-                        <img src={IconPhone} alt="dumbledore" className="icon icon-phone" />
+                        <IconPhone className='icon' />
+                        {/* <svg src={IconPhone} alt="dumbledore" className="icon icon-phone" /> */}
                         <span className='feature-txt'>View your house scoreboard anytime, anywhere</span>
                     </div>
                     <div className='feature-2 feature'>
-                        <img src={IconTeacher} alt="dumbledore" className="icon icon-teacher" />
+                        <IconTeacher className='icon' />
                         <span className='feature-txt'>Coordinate with all the teachers</span>
                     </div>
                     <div className='feature-3 feature'>
-                        <img src={IconChart} alt="dumbledore" className="icon icon-teacher" />
+                        <IconChart className='icon' />
                         <span className='feature-txt'>Visual analysis at your fingertip</span>
                     </div>
                 </div>
                 <div className='landing-page-block landing-page-block-3'>
-                    <h2>Check out the top-tier schools we work with</h2>
+                    <h2 className='checkout'>Check out the top-tier schools we work with</h2>
                     <div className='schools-list'>
                         {this.state.schoolsList.map((school) => {
                             return (
-                                <h2>{school.name}</h2>
+                                <h2 className='school'>{school.name}</h2>
                             )
                         })}
                     </div>
