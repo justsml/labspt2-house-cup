633804f54be83e9ed4885071e87e8c3d8a090c69
louislee7 <7.louislee@gmail.com>
2019-04-08 18:47:23 -0700

    new pr

--- HouseCup/house-cup-app/src/sub-components/landingPage.js
+++ HouseCup/house-cup-app/src/sub-components/landingPage.js
@@ -7,7 +7,7 @@ import { ReactComponent as IconTeacher } from '../images/icon-teacher.svg';
 import { ReactComponent as IconCastle } from '../images/castle.svg';
 
 //GSAP import
-import { TweenLite } from "gsap/all";
+import { TweenMax, Linear } from "gsap/all";
 
 import schoolsTestData from '../mock data/schools';
 import auth from '../Auth';
@@ -15,10 +15,6 @@ import auth from '../Auth';
 class LandingPage extends React.Component {
     constructor(props) {
         super(props);
-        // reference to the DOM node
-        this.myElement = null;
-        // reference to the animation
-        this.myTween = null;
         this.state = {
             username: "",
             password: "",
@@ -30,8 +26,16 @@ class LandingPage extends React.Component {
         this.setState({
             schoolsList: schoolsTestData
         });
-        this.myTween = TweenLite.to(this.myElement, 1, { x: 100, repeat: -1 });
-
+        TweenMax.fromTo(".school-box", 2, { x: -1680 }, {
+            x: "+=2600", //move each box 500px to right
+            ease: Linear.easeNone,
+            modifiers: {
+                x: function (x) {
+                    return x % 2600; //force x value to be between 0 and 500 using modulus
+                }
+            },
+            repeat: -1
+        });
     }
 
     handleInput = e => {
@@ -99,7 +103,7 @@ class LandingPage extends React.Component {
                 </div>
                 <div className='landing-page-block landing-page-block-3'>
                     <h2 className='checkout'>Check out the top-tier schools we work with</h2>
-                    <div className='schools-list' ref={div => this.myElement = div} >
+                    <div className='schools-list' >
                         {this.state.schoolsList.map((school) => {
                             return (
                                 <div className={`school-box school-box-${school.id}`} >
@@ -125,7 +129,9 @@ class LandingPage extends React.Component {
                 </div>
             </div>
         )
+
     }
+
 }
 
 export default LandingPage;
