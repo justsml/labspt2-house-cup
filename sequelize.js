5365e09c3b4b165a4d383f7b9cae3511e8f71c7f
Dan Levy <397632+justsml@users.noreply.github.com>
2019-05-14 20:01:30 -0600

    Verify your db connection

--- sequelize.js
+++ sequelize.js
@@ -4,4 +4,14 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:'postgres'
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Sequelize: Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('ERROR: Sequelize was unable to connect to the database:', err);
+  });
+
 module.exports = sequelize;
